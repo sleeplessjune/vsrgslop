cdb5e2e4b7fcdc02b3f056bbfe5c28864967bbd8
lesbianswordfighting <personallymara@gmail.com>
2025-12-11 11:26:41 -0800

    chart started + song change
    starting work w charting and changed song

---
+++ scripts/chart.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+const in_editor: bool = false
+var song_name = "OVERJOY OVERDOSE"
+
+func _ready():
+	if in_editor:
+		pass 
--- scripts/note_receptor.gd
+++ scripts/note_receptor.gd
@@ -1,23 +1,14 @@
 extends AnimatedSprite2D
 
-#note creation on keypress function by Luera: https://www.youtube.com/watch?v=bXOTSNvchWI
 
 @export var key_name: String = ""
 @onready var note_receptor: AnimatedSprite2D = $"."
-@onready var note = preload("res://scenes/note.tscn")
 
 
 # handles note receptor state (not/pressed)
 func _process(delta):
 	if Input.is_action_just_pressed(key_name):
-		CreateNote()
 		note_receptor.play("pressed")
 	if Input.is_action_just_released(key_name):
 		note_receptor.play("not_pressed")
-
-# temporary note creation process for testing
-func CreateNote():
-	var fk_inst = note.instantiate()
-	get_tree().get_root().call_deferred("add_child", fk_inst)
-	fk_inst.Setup(position.x)
 	
